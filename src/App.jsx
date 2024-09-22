761eeea25746c74e57c4d9dfa1d78e966174e70e
Venkat Swaraj <sathiraju829@gmail.com>
2024-09-22 18:23:27 +0530

    Added a simple cube and orbital controls

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
 
 import './App.css'
 import { Canvas } from '@react-three/fiber'
+import { Experience } from './components/Experience'
 
 function App() {
   return (
     <Canvas>
-
+      <Experience />
     </Canvas>
   )
 }
